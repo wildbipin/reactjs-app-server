435c0697b3426a5b1df1c1b212c5da3748ee5df9
Bipin Meena <98609629+wildbipin@users.noreply.github.com>
2023-07-28 23:34:44 +0530

    Update mongodb connection

--- db/index.js
+++ db/index.js
@@ -1,14 +1,21 @@
 const mongoose = require('mongoose');
 
-const connectionString = process.env.DB_URL;
+//const connectionString = process.env.DB_URL;
 
-//mongoose.connect(connectionString, { useNewUrlParser: true }).catch((e) => {
-//  console.error('Connection error', e.message);
-//});
+//const connectionString = mongodb://<username>:<password>@<pod-0-name>.<headless-service-name>.<namespace>.svc.cluster.local:27017,<pod-1-name>.<headless-service-name>.<namespace>.svc.cluster.local:27017/<database-name>?replicaSet=rs0;
 
-mongoose.connect('mongodb://data/db', { useNewUrlParser: true }).catch((e) => {
+
+const connectionString = mongodb://adminuser:password123@mongodb-0.mongodb-service.default.svc.cluster.local:27017,mongodb-1.mongodb-service.default.svc.cluster.local:27017/moviedb?replicaSet=mongo-rs;
+
+mongoose.connect(connectionString, { 
+  useNewUrlParser: true,
+  useUnifiedTopology: true,}).catch((e) => {
   console.error('Connection error', e.message);
 });
+
+//mongoose.connect('mongodb://data/db', { useNewUrlParser: true }).catch((e) => {
+//  console.error('Connection error', e.message);
+//});
 
 const db = mongoose.connection;
 
