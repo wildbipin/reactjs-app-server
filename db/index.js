4b10b3936aef9c918eb05bea43571b9c36d69569
Bipin Meena <98609629+wildbipin@users.noreply.github.com>
2023-09-01 19:19:32 +0530

    Update index.js

--- db/index.js
+++ db/index.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 const mongoUri = 'mongodb://reactjsstaticwebapp-server:cXNOMD6RkXMaPeSX4nXt7JR6aZVUqObw1gfBo8UOsOGMXfkY0glhelfqxxWkd8jJZ1sGlEjbZYAMACDbO5HUkQ==@reactjsstaticwebapp-server.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@reactjsstaticwebapp-server@';
 
 mongoose.connect(mongoUri, { 
-  useNewUrlParser: true,
-  useUnifiedTopology: true,}).catch((e) => {
+  useNewUrlParser: true }).catch((e) => {
   console.error('Connection error', e.message);
 });
 
