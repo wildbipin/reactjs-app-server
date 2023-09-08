cf92357c155e1dfb4a759038b35405fecd441ed8
Bipin Meena <98609629+wildbipin@users.noreply.github.com>
2023-09-08 18:42:46 +0530

    Update index.js

--- db/index.js
+++ db/index.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
-const mongoUri = 'mongodb://reactjsstaticwebapp-server:cXNOMD6RkXMaPeSX4nXt7JR6aZVUqObw1gfBo8UOsOGMXfkY0glhelfqxxWkd8jJZ1sGlEjbZYAMACDbO5HUkQ==@reactjsstaticwebapp-server.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@reactjsstaticwebapp-server@';
-
+//const mongoUri = 'mongodb://reactjsstaticwebapp-server:cXNOMD6RkXMaPeSX4nXt7JR6aZVUqObw1gfBo8UOsOGMXfkY0glhelfqxxWkd8jJZ1sGlEjbZYAMACDbO5HUkQ==@reactjsstaticwebapp-server.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@reactjsstaticwebapp-server@';
+const mongoUri = 'mongodb://reactjsstaticwebapp-server:cXNOMD6RkXMaPeSX4nXt7JR6aZVUqObw1gfBo8UOsOGMXfkY0glhelfqxxWkd8jJZ1sGlEjbZYAMACDbO5HUkQ==@reactjsstaticwebapp-server.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@reactjsstaticwebapp-server@';
 mongoose.connect(mongoUri, { 
-  useNewUrlParser: true }).catch((e) => {
+  useNewUrlParser: true, useUnifiedTopology: true }).catch((e) => {
   console.error('Connection error', e.message);
 });
 
